1cedb1c68c0e508c8d06f3aa5b4589da326e09d0
Mel Incognito <melxincognito@gmail.com>
2021-11-18 00:12:01 -0700

    creating initial layout w JSX

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className='App-header'>Volvemos a Madrid!</header>
-      <div> </div>
-    
+      <header className='App-header'>Lista de Tareas</header>
+      <div className='content'>
+        <div className='myLists'> 
+          <button> Add List </button> 
+          <h3> Tareas </h3>
+          <h3> De Casa </h3>
+          <h3> Peques </h3>
+        </div>
+        <div className='todos'> 
+          <h2> Mi Lista </h2>
+          <p> Haz esto </p>
+        </div>
+        <div className='finishedTodos'> 
+          <h2> Terminado </h2>
+          <p> Ya hicistes esto </p>
+        </div>
+      </div>
     </div>
   );
 }
