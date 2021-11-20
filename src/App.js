e4868b1e15f2031ce14c26f3d5030438c903f5bf
Mel Incognito <melxincognito@gmail.com>
2021-11-20 15:53:01 -0700

    form rendering on correct area

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import Card from "./components/UI/card/card.component";
 import NewTodoItem from "./components/todos/new-todo/newTodo.component";
 import MyLists from "./components/lists/listLayout.module";
-
+import { React, useState } from "react";
+import NewTodoForm from "./components/forms/todos/newTodoForm.component";
+import Backdrop from "./components/UI/layout/backdrop.component";
 
 function App() {
-  function tali() {
-    alert('puta');
+  const [showModal, setModalIsOpen] = useState(false);
+  function addTodo() {
+    setModalIsOpen(true);
   }
-  
+
+  function closeModule() {
+    setModalIsOpen(false);
+  }
 
   return (
     <div className="App">
@@ -19,22 +25,27 @@ function App() {
           <h1> Mi Lista </h1>
           <div className="myTodos">
             <Card>
-              <NewTodoItem title='Encontrar Trabajo' description='Encontrar trabajo en Madrid creando aplicaciones para el web' fecha='03/07/2022' />
+              <NewTodoItem
+                title="Encontrar Trabajo"
+                description="Encontrar trabajo en Madrid creando aplicaciones para el web"
+                fecha="03/07/2022"
+              />
             </Card>
-            <Card >
-              <NewTodoItem title='Mudanza' description='Volvemos a España porque no podemos correr de nuestros problemas para siempre' fecha='03/15/2022' />
+            <Card>
+              <NewTodoItem
+                title="Mudanza"
+                description="Volvemos a España porque no podemos correr de nuestros problemas para siempre"
+                fecha="03/15/2022"
+              />
             </Card>
             <div className="addItem">
-              <button className="addItemBtn" title="Añadir Nuevo Recuerdo" onClick={tali}>
+              <button className="addItemBtn" title="Añadir Nuevo Recuerdo" onClick={addTodo}>
                 {" "}
                 +{" "}
               </button>
-
-
             </div>
-          
-         
-            
+            {showModal && <NewTodoForm />}
+            {showModal && <Backdrop onClick={closeModule} />}
           </div>
         </div>
       </div>
