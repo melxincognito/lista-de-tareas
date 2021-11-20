c8546517011ed28c1e65377a4c113d03539a0203
Mel Incognito <melxincognito@gmail.com>
2021-11-20 12:02:41 -0700

    working on rendering dynamic content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Card from "./components/todos/card/card.component";
+import Card from "./components/UI/card/card.component";
 import NewTodoItem from "./components/todos/new-todo/newTodo.component";
 import MyLists from "./components/lists/listLayout.module";
 
@@ -18,7 +18,7 @@ function App() {
           <h1> Mi Lista </h1>
           <div className="myTodos">
             <Card>
-              <NewTodoItem />
+              <NewTodoItem title='Encontrar Trabajo' description='Encontrar trabajo en Madrid creando aplicaciones para el web' fecha='03/07/2022' />
             </Card>
             <Card>
               <NewTodoItem />
